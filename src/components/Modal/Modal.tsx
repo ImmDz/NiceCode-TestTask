d5ff96f9736544521d8a5f4c445541e11da699c8
ImmDz <johnbessmerntyjj@gmail.com>
2023-10-10 16:42:00 +0300

    fixes part 1

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -2,6 +2,9 @@ import { v4 as uuidv4 } from "uuid";
 import { useForm } from "react-hook-form";
 import nofoto from "../../assets/patientFoto/nofoto.png";
 import cross from "../../assets/icons/cross.svg";
+import { Input } from "../Input/Input";
+import { namePattern } from "../../utils/validationPatterns";
+import { formHelpers } from "../../utils/formHelpers";
 
 import useAppDispatch from "../../hooks/useAppSelector";
 import { toggle } from "../../redux/modal/modalSlice";
@@ -12,78 +15,93 @@ import { IPatient, IModal } from "../../types/general";
 import style from "./Modal.module.css";
 
 export const Modal = ({ isOpen }: IModal) => {
-    const dispatch = useAppDispatch();
+	const dispatch = useAppDispatch();
+	const defaultPatient = {
+		id: uuidv4(),
+		name: "",
+		surname: "",
+		img: nofoto,
+		icon: "",
+		age: 0,
+		gender: "",
+		notes: [],
+		consultations: [],
+		videos: [],
+		events: [],
+	};
 
-    const defaultPatient = {
-        id: uuidv4(),
-        name: "",
-        surname: "",
-        img: nofoto,
-        icon: "",
-        age: undefined,
-        gender: "",
-        notes: [],
-        consultations: [],
-        videos: [],
-        events: [],
-    };
+	const {
+		handleSubmit,
+		reset,
+		control,
+		register,
+		setValue,
+		formState: { isValid, errors },
+	} = useForm<IPatient>({
+		defaultValues: defaultPatient,
+		mode: "all",
+		delayError: 300,
+	});
 
-    const {
-        handleSubmit,
-        reset,
-        control,
-        register,
-        setValue,
-    } = useForm<IPatient>({
-        defaultValues: defaultPatient,
-        mode: "all",
-        delayError: 300,
-    });
+	const submitHandler = () => {
+		if (isValid) {
+			setValue("id", uuidv4());
+			dispatch(addPatient(control._formValues as IPatient));
+			reset();
+			dispatch(toggle(false));
+		}
+	};
 
-    const submitHandler = () => {
-        setValue("id", uuidv4());
-        dispatch(addPatient(control._formValues as IPatient));
-        reset();
-        dispatch(toggle(false));
-    };
-
-    return (
-        <dialog className={style.modalWrapper} open={isOpen}>
-            <div className={style.modalContainer}>
-                <h3>Добавить пациента</h3>
-                <span className={style.cross} onClick={() => dispatch(toggle(false))}>
-                    <img src={cross} alt="Закрыть" />
-                </span>
-                <form className={style.modalForm} action="" onSubmit={handleSubmit(submitHandler)}>
-                    <input
-                        required
-                        type="text"
-                        placeholder="Имя"
-                        {...register("name")} />
-                    <input
-                        required
-                        type="text"
-                        placeholder="Фамилия"
-                        {...register("surname")} />
-                    <input
-                        required
-                        type="number"
-                        placeholder="Возраст"
-                        {...register("age")} />
-                    <div className={style.genderPart}>
-                        <p className={style.genderPartTitle}>Пол:</p>
-                        <label htmlFor="male">
-                            <input id="male" type="radio" value="муж" required {...register("gender")} />
-                            мужской
-                        </label>
-                        <label htmlFor="female">
-                            <input id="female" type="radio" value="жен" {...register("gender")} />
-                            женский
-                        </label>
-                    </div>
-                    <button type="submit">Добавить</button>
-                </form>
-            </div>
-        </dialog>
-    )
+	return (
+		<dialog className={style.modalWrapper} open={isOpen}>
+			<div className={style.modalContainer}>
+				<h3 className={style.formTitle}>Добавить пациента</h3>
+				<span className={style.cross} onClick={() => dispatch(toggle(false))}>
+					<img src={cross} alt="Закрыть" />
+				</span>
+				<form className={style.modalForm} action="" onSubmit={handleSubmit(submitHandler)}>
+					<label>
+						<p className={style.inputTitle}>*Имя</p>
+						<Input
+							name={"name"}
+							control={control}
+							rules={{ pattern: namePattern, minLength: 2, maxLength: 15, required: true }}
+						/>
+						<p className={style.message}>{formHelpers.getNameError(errors)}</p>
+					</label>
+					<label>
+						<p className={style.inputTitle}>*Фамилия</p>
+						<Input
+							name={"surname"}
+							control={control}
+							rules={{ minLength: 2, maxLength: 15, pattern: namePattern, required: true }}
+						/>
+						<p className={style.message}>{formHelpers.getSurnameError(errors)}</p>
+					</label>
+					<label>
+						<p className={style.inputTitle}>*Возраст</p>
+						<Input
+							name={"age"}
+							control={control}
+							type="number"
+							rules={{ required: true, min: 12, max: 120 }}
+						/>
+						<p className={style.message}>{formHelpers.getAgeError(errors)}</p>
+					</label>
+					<div className={style.genderPart}>
+						<p className={style.genderPartTitle}>Пол:</p>
+						<label htmlFor="male">
+							<input id="male" defaultChecked type="radio" value="муж" {...register("gender")} />
+							мужской
+						</label>
+						<label htmlFor="female">
+							<input id="female" type="radio" value="жен" {...register("gender")} />
+							женский
+						</label>
+					</div>
+					<button type="submit">Добавить</button>
+				</form>
+			</div>
+		</dialog>
+	);
 };
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,25 +3,27 @@ import classNames from "classnames";
 import style from "./Navbar.module.css";
 
 interface INavbar {
-    content: string[];
-    activeTab: string;
-    setActiveTab: (tab: string) => void;
+	content: string[];
+	activeTab: string;
+	setActiveTab: (tab: string) => void;
 }
 
 export const Navbar = ({ content, activeTab, setActiveTab }: INavbar) => {
-    const handleClick = (tab: string) => {
-        setActiveTab(tab);
-    };
+	const handleClick = (tab: string) => {
+		setActiveTab(tab);
+	};
 
-    return (
-        <ul className={style.navbar}>
-            {content.map((tab, index) =>
-                <li
-                    key={index}
-                    onClick={() => handleClick(tab)}
-                    className={classNames(style.tab, { [style.active]: activeTab === tab })}>
-                    {tab}
-                </li>)}
-        </ul>
-    )
+	return (
+		<ul className={style.navbar}>
+			{content.map((tab, index) => (
+				<li
+					key={index}
+					onClick={() => handleClick(tab)}
+					className={classNames(style.tab, { [style.active]: activeTab === tab })}
+				>
+					{tab}
+				</li>
+			))}
+		</ul>
+	);
 };
